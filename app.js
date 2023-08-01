ca02082e21401b88869a2f16a5949754092c54fa
tarunsharma17 <tarunsharma176090@gmail.com>
2023-08-01 12:26:33 +0530

    modified listen port code

--- app.js
+++ app.js
@@ -226,6 +226,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}!!!!!!!!!`);
 });
